d6cb714f97b4a190244a6871993fe48d7e509082
Garret29 <szymon.piotrowski93@gmail.com>
2019-01-29 15:55:44 +0100

    added plot with relation between alpha and slope coefficient

--- regression_models.py
+++ regression_models.py
@@ -15,7 +15,7 @@ boston_data['Price'] = boston.target
 X = boston_data['NOX'].values[:, np.newaxis]
 Y = boston_data['Price'].values
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.15)
 
 linearRegression = LinearRegression()
 linearRegression.fit(X_train, Y_train)
@@ -31,6 +31,14 @@ lassoRegression.fit(X_train, Y_train)
 elasticNetRegression = ElasticNet(alpha=alpha, l1_ratio=0.8)
 elasticNetRegression.fit(X_train, Y_train)
 
+models = {"linearRegression": linearRegression, "ridgeRegression": ridgeRegression, "lassoRegression": lassoRegression,
+          "elasticNetRegression": elasticNetRegression}
+
+for key in models:
+    print(key, ": ", "Współczynnik determinacji dla zbioru testowego: ", models[key].score(X_test, Y_test),
+          ", Współczynnik determinacji dla zbioru uczącego: ", models[key].score(X_train, Y_train),
+          ", Współczynnik 'a' prostej regresji: ", models[key].coef_)
+
 plt.scatter(X_train, Y_train, color='r')
 plt.plot(X_train, linearRegression.predict(X_train), color='k')
 plt.show()
@@ -45,4 +53,41 @@ plt.show()
 
 plt.scatter(X_train, Y_train, color='y')
 plt.plot(X_train, elasticNetRegression.predict(X_train), color='k')
+plt.show()
+
+print("--------------------------------------------------")
+
+alphas = []
+ridgeCoefs = []
+lassoCoefs = []
+elasticNetCoefs = []
+
+for alpha in range(1, 51):
+
+    newAlpha = alpha/10
+    alphas.append(newAlpha)
+
+    ridgeRegression = Ridge(alpha=newAlpha)
+    ridgeRegression.fit(X_train, Y_train)
+
+    ridgeCoefs.append(ridgeRegression.coef_[0])
+
+    lassoRegression = Lasso(alpha=newAlpha)
+    lassoRegression.fit(X_train, Y_train)
+
+    lassoCoefs.append(lassoRegression.coef_[0])
+
+    elasticNetRegression = ElasticNet(alpha=newAlpha, l1_ratio=0.8)
+    elasticNetRegression.fit(X_train, Y_train)
+
+    elasticNetCoefs.append(elasticNetRegression.coef_[0])
+
+    models = {"linearRegression": linearRegression, "ridgeRegression": ridgeRegression,
+              "lassoRegression": lassoRegression,
+              "elasticNetRegression": elasticNetRegression}
+
+plt.plot(alphas, ridgeCoefs, color="r", label="ridge")
+plt.plot(alphas, lassoCoefs, color="g", label="lasso")
+plt.plot(alphas, elasticNetCoefs, color="b", label="elastic net")
+plt.legend()
 plt.show()
